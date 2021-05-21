92b0f435518934e9f6e0e95e650aafc1ddb76944
Marcel Subirana <msubiranac@gmail.com>
2021-05-21 18:25:02 +0200

    test(helper tests): add tests for all helper methods

---
+++ src/helpers/helpers.mock.js
@@ -0,0 +1,40 @@
+const mockRepoList = [
+  {
+    name: 'google-login',
+    url: 'https://github.com/Marcel2408/google-login',
+    description: null,
+    updatedAt: '2021-04-26T07:21:20Z',
+    id: 'MDEwOlJlcG9zaXRvcnkzNjE2NTEzMjM=',
+    stargazerCount: 0,
+    primaryLanguage: {
+      name: 'TypeScript',
+      color: '#2b7489',
+    },
+  },
+  {
+    name: 'ng-form',
+    url: 'https://github.com/Marcel2408/ng-form',
+    description: null,
+    updatedAt: '2021-05-06T22:30:04Z',
+    id: 'MDEwOlJlcG9zaXRvcnkzNjUwNTE3MjQ=',
+    stargazerCount: 0,
+    primaryLanguage: {
+      name: 'TypeScript',
+      color: '#2b7489',
+    },
+  },
+  {
+    name: 'mvst-fe',
+    url: 'https://github.com/Marcel2408/mvst-fe',
+    description: null,
+    updatedAt: '2021-05-20T06:18:21Z',
+    id: 'MDEwOlJlcG9zaXRvcnkzNjg2MDQ5NzQ=',
+    stargazerCount: 0,
+    primaryLanguage: {
+      name: 'JavaScript',
+      color: '#f1e05a',
+    },
+  },
+];
+
+export default mockRepoList;
--- src/helpers/helpers.test.js
+++ src/helpers/helpers.test.js
@@ -1,24 +1,61 @@
+import moment from 'moment';
 import debounce from './debounce';
+import mockRepoList from './helpers.mock';
+import reposSortByDate from './reposSortByDate';
+import timeDiff from './timeDiff';
 
 describe('debounce', () => {
+  const fn = jest.fn();
+  const delay = 500;
+  const debounceFn = debounce(fn, delay);
   beforeEach(() => {
     jest.useFakeTimers();
   });
   test('debounced fn to not been called before specified delay', () => {
-    const fn = jest.fn();
-    const delay = 500;
-    const debounceFn = debounce(fn, delay);
     debounceFn();
     jest.advanceTimersByTime(300);
     expect(fn).toBeCalledTimes(0);
   });
 
   test('debounced fn call after the specified delay', () => {
-    const fn = jest.fn();
-    const delay = 500;
-    const debounceFn = debounce(fn, delay);
     debounceFn();
     jest.advanceTimersByTime(700);
     expect(fn).toBeCalledTimes(1);
+  });
+});
+
+describe('timeDiff', () => {
+  test('if time is not from this year return time in "MMM D, YYYY"', () => {
+    const time = '2019-04-26T07:21:20Z';
+    const formatedTime = timeDiff(time);
+    const timeIfNotThisYearFormat = `on ${moment(time).format('MMM D, YYYY')}`;
+    expect(formatedTime).toEqual(timeIfNotThisYearFormat);
+  });
+
+  test('if time is this year and more than 29 days ago return time in "MMM D"', () => {
+    const time = '2021-04-07T07:21:20Z';
+    const formatedTime = timeDiff(time);
+    const timeIfNotThisYearFormat = `on ${moment(time).format('MMM D')}`;
+    expect(formatedTime).toEqual(timeIfNotThisYearFormat);
+  });
+
+  test('if time is this year and less than 29 days ago return time "from now"', () => {
+    const time = '2021-05-07T07:21:20Z';
+    const formatedTime = timeDiff(time);
+    const timeIfNotThisYearFormat = moment(time).fromNow();
+    expect(formatedTime).toEqual(timeIfNotThisYearFormat);
+  });
+});
+
+describe('reposSortByDate', () => {
+  test('correctly sorts by most recent updated repo', () => {
+    const originalRepoNames = `${mockRepoList[0].name}, ${mockRepoList[1].name}, ${mockRepoList[2].name}`;
+
+    const sortedMockRepoList = reposSortByDate(mockRepoList);
+
+    const sortedRepoNames = `${sortedMockRepoList[0].name}, ${sortedMockRepoList[1].name}, ${sortedMockRepoList[2].name}`;
+
+    expect(originalRepoNames).toEqual('google-login, ng-form, mvst-fe');
+    expect(sortedRepoNames).toEqual('mvst-fe, ng-form, google-login');
   });
 });
